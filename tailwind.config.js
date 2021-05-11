046dbb1cf04e371608a461f37d194d0eeec36b68
ifilipe.lype <filipe7lype@gmail.com>
2021-05-11 08:55:05 +0100

    adds index-page colors from figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          1: "#F6F8FB",
+          2: "#4F4F4F",
+          3: "#828282",
+          4: "#BDBDBD"
+        },
+        blue: "#97BEF4",
+      }
+    },
   },
   variants: {
     extend: {},
