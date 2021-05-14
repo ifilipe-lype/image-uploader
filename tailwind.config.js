b92e063aefe4435fe1ffaf23252a83c71ea0972b
ifilipe.lype <filipe7lype@gmail.com>
2021-05-14 07:05:34 +0100

    adds colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,15 @@ module.exports = {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
       colors: {
+        green: { 1: "#219653"},
         gray: {
           1: "#F6F8FB",
           2: "#4F4F4F",
           3: "#828282",
           4: "#BDBDBD",
           5: "#A9A9A9",
-          6: "#F2F2F2"
+          6: "#F2F2F2",
+          7: "#E0E0E0"
         },
         blue: "#97BEF4",
         blue1: "#2F80ED",
