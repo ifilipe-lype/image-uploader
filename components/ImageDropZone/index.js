759e99db66024837dfe7f1ea31a4f1c916f13000
ifilipe.lype <filipe7lype@gmail.com>
2021-05-15 14:29:22 +0100

    adds feedback to drag-drop feature

--- components/ImageDropZone/index.js
+++ components/ImageDropZone/index.js
@@ -5,24 +5,38 @@ export default function ImageDropZone({ setDropFile }) {
   const onDrop = useCallback((acceptedFiles) => {
     setDropFile(acceptedFiles[0]);
   }, []);
+
   const {
     getRootProps,
     isDragActive,
     isDragAccept,
+    isDragReject
   } = useDropzone({
     onDrop,
     noClick: true,
     maxFiles: 1,
-    accept: "image/jpeg, image/png, image/svg+xml, image/jpg",
+    maxSize: 100,
+    accept: "image/*",
   });
 
   return (
     <div {...getRootProps()}
-      className={`flex flex-col items-center outline-none justify-center border-dashed border-2 border-blue px-4 py-8 rounded-xl bg-gray-1 transition ${isDragActive ? "bg-green-100" : ""}`}
+      className={`flex flex-col items-center outline-none justify-center border-dashed border-2 px-4 py-8 rounded-xl bg-gray-1 transition ${isDragAccept ? "bg-green-100" : ""} ${isDragReject ? "bg-red-100" : ""} ${isDragReject ? "border-red-200" : "border-blue"}`}
     >
       <img src="/assets/image.svg" className="w-auto h-auto object-contain" />
       <p className="text-gray-4 mt-8 text-xs">
-        { !isDragActive && !isDragAccept ? (<span>Drag &amp; Drop your image here</span>) : <span className="text-blue1">Release to upload image</span>}
+        {
+
+          isDragAccept ? (
+            <span className="text-blue1">Release to upload image</span>
+          ) : (
+            isDragActive && isDragReject ? (
+              <span className="text-red-400">file selected is not an image</span>
+            ) : (
+              <span className="text-gray-2">Drag &amp; Drop an image file here to upload</span>
+            )
+          )
+        }
       </p>
     </div>
   );
