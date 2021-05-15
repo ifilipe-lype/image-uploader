e5b81c9aa8bbce9e67c882ba41c14753d842f303
ifilipe.lype <filipe7lype@gmail.com>
2021-05-15 13:23:11 +0100

    adds copy feature and changes check svg icon

--- components/Preview/index.js
+++ components/Preview/index.js
@@ -1,9 +1,26 @@
+import { useRef, useState, useEffect } from "react";
 
-export default function Preview({ imgSrc }){
+export default function Preview({ imgSrc }) {
+    const [copied, setCopied] = useState(false);
+    const inputRef = useRef(null);
+
+
+
+
+    function copyUrl() {
+        const inputElem = inputRef.current;
+        console.log(inputElem.value)
+        inputElem.focus();
+        inputElem.select()
+        inputElem.setSelectionRange(0, 99999); /* For mobile devices */
+        document.execCommand("copy");
+        setCopied(true);
+    }
+
     return (
         <div className="w-full max-w-md flex flex-col items-center justify-center bg-white p-8 splash-shadow rounded-xl">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-green-1" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-12 w-12 text-green-1" fill="currentColor">
+                <path d="M0 0h24v24H0z" fill="none" /><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
             </svg>
             <h3 className="text-xl font-medium text-gray-2 mb-4">
                 Uploaded successfully!
@@ -13,8 +30,15 @@ export default function Preview({ imgSrc }){
             </div>
 
             <div className="w-full p-1 rounded-lg flex-nowrap border border-gray-7 bg-gray-1 flex items-center justify-between">
+                <input type="text" value={imgSrc} className="sr-only" readOnly ref={inputRef} />
                 <span className="text-xs pl-2 w-8/12 overflow-hidden text-left whitespace-nowrap text-gray-2 overflow-ellipsis">{imgSrc}</span>
-                <button className="bg-blue1 px-4 py-2 text-white rounded-lg text-sm font-normal flex-nowrap">Copy Link</button>
+                <button onClick={copyUrl} className="bg-blue1 px-4 py-2 text-white rounded-lg text-sm font-normal flex-nowrap">
+                    Copy Link
+                </button>
+            </div>
+
+            <div className="mt-4 text-xs md:text-sm text-blue1">
+                {copied && <p>Image's link copied to the clipboard</p>}
             </div>
         </div>
     )
